21d57fe0cb43816661590c49c9d18177aa0dae82
lhg0529 <lhg0529@naver.com>
2023-08-22 17:16:27 +0900

    second

---
+++ src/ch02/VariableInitializationExample.java
@@ -0,0 +1,10 @@
+package ch02;
+
+public class VariableInitializationExample {
+  public static void main(String[] args) {
+    int value = 1;
+    int result = value + 10;
+
+    System.out.println("result 값은 " +result + "입니다");
+  }
+}
---
+++ src/ch02/Vv.java
@@ -0,0 +1,10 @@
+package ch02;
+
+public class Vv {
+  public static void main(String[] args) {
+    int x = 10;
+    boolean result = ( x < 0 || x > 200);
+    System.out.println(result);
+  }
+}
+//123123
